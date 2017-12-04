e4e826d91fb8d0c3db5700f3155cbc0c112a6111
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-12-04 17:15:31 +0000

    route to startvisitPage implemented

--- src/elm/Routes/Home.elm
+++ src/elm/Routes/Home.elm
@@ -9,6 +9,6 @@ import Model exposing (..)
 home : Model -> Html Msg
 home model =
     div [ class "w-60-ns center" ]
-        [ h1 [ class "tc f1" ] [ text "hello" ]
-        , p [ class "f3 w60 mh1 tc" ] [ text "Welcome to this SPA elm boiler plate. Tachyons is included. Go nuts." ]
+        [ h1 [ class "tc f1" ] [ text "hello Janet!" ]
+        , a [ href "#startVisitPage" ] [ button [ class "f3" ] [ text "I'm in" ] ]
         ]
