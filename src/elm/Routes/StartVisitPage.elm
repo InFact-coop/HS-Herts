2e862dfa4688f87697777b9a09ffa10b8211ada0
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-12-08 12:15:34 +0000

    bits and bobs

--- src/elm/Routes/StartVisitPage.elm
+++ src/elm/Routes/StartVisitPage.elm
@@ -13,5 +13,13 @@ startVisitPage model =
         [ img [ class "center pt5 h5 ma0", src "./assets/logo.png" ] []
         , p [ class "f3 w60 mh1 tc" ] [ text <| convertToMin model.timerLength ]
         , a [ href "#stopVisitQuestionPage", class "link" ] [ text "End Visit" ]
-        , button [ class "ma3 brand bg-white center db f3", onClick CancelVisit ] [ text "Cancel this visit" ]
+        , section [ class "center ma0 mt2 pt5" ] [ button [ class "ma3 brand bg-white center db f3", onClick CancelVisit ] [ text "Cancel this visit" ] ]
         ]
+
+
+
+--
+-- , section [ class "center ma0 mt2 pt5" ]
+--     [ button [ class "br1 w8 h8 ma3 brand bg-white center db f3 b-orange", onClick StartVisit ] [ text "Start my visit" ]
+--     , a [ href "#previousVisitsOverviewPage", class "link" ] [ button [ class "br1 w8 h8 ma3 brand-pink bg-white center db f3 prevVisits" ] [ text "Go to visits" ] ]
+--     ]
