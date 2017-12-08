587262b8cc0a63eeb8ca311b8ee00b86358aaf1a
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-12-08 12:35:24 +0000

    started secondPage styling

--- src/elm/Routes/StartVisitPage.elm
+++ src/elm/Routes/StartVisitPage.elm
@@ -12,14 +12,8 @@ startVisitPage model =
     div [ class "w-60-ns center" ]
         [ img [ class "center pt5 h5 ma0", src "./assets/logo.png" ] []
         , p [ class "f3 w60 mh1 tc" ] [ text <| convertToMin model.timerLength ]
-        , a [ href "#stopVisitQuestionPage", class "link" ] [ text "End Visit" ]
-        , section [ class "center ma0 mt2 pt5" ] [ button [ class "ma3 brand bg-white center db f3", onClick CancelVisit ] [ text "Cancel this visit" ] ]
+        , section [ class "center ma0 mt2 pt5" ]
+            [ a [ href "#stopVisitQuestionPage", class "link" ] [ button [ class "br1 h3 w7 ma3 brand bg-white center db f4 b-orange" ] [ text "Finish my visit" ] ]
+            , section [ class "center ma0 mt2" ] [ button [ class "h3 w7 br1 ma3 brand-pink bg-white center db f4 br-pink ", onClick CancelVisit ] [ text "Cancel the visit" ] ]
+            ]
         ]
-
-
-
---
--- , section [ class "center ma0 mt2 pt5" ]
---     [ button [ class "br1 w8 h8 ma3 brand bg-white center db f3 b-orange", onClick StartVisit ] [ text "Start my visit" ]
---     , a [ href "#previousVisitsOverviewPage", class "link" ] [ button [ class "br1 w8 h8 ma3 brand-pink bg-white center db f3 prevVisits" ] [ text "Go to visits" ] ]
---     ]
