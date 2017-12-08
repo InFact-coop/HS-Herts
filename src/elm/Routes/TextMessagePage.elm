630a9ec492356debd8911bad1748445e7d1e7c5e
Matt Davis <matthewpeterdavis@gmail.com>
2017-12-08 16:35:54 +0000

    style text input page

--- src/elm/Routes/TextMessagePage.elm
+++ src/elm/Routes/TextMessagePage.elm
@@ -17,8 +17,9 @@ textMessagePage model =
                 Nothing ->
                     ""
     in
-    div [ class "w-60-ns center" ]
-        [ p [ class "f3 w60 mh1 tc" ] [ text "Notes" ]
-        , div [] [ textarea [ class "h-100 w-100 input-reset", onInput UpdateTextFeedback, value textValue, placeholder "Enter feedback here..." ] [] ]
-        , a [ href "#stopVisitPage" ] [ button [] [ text "Done" ] ]
+    div [ class "w-60-ns center tc" ]
+        [ div [ class "w-100 tl ma2 pa2" ] [ img [ class "h3", onClick GoBack, src "./assets/back_btn.svg" ] [] ]
+        , img [ class "vh-15 pa3 br-100", src "./assets/write.svg" ] []
+        , div [ class "pa3" ] [ textarea [ class "vh-25 w-100 input-reset pa3 f4 br3 b-orange bg--black-40", onInput UpdateTextFeedback, value textValue, placeholder "Enter feedback here..." ] [] ]
+        , a [ href "#stopVisitPage" ] [ button [ class "dim b pointer w7 h8 br1 f3 ma2  bg--bp white" ] [ text "Submit" ] ]
         ]
