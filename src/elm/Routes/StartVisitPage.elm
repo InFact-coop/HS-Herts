8aa2255a1456b0101938cc6b0b25fb11b2d71164
Matt Davis <matthewpeterdavis@gmail.com>
2017-12-10 13:41:09 +0000

    format the clock

--- src/elm/Routes/StartVisitPage.elm
+++ src/elm/Routes/StartVisitPage.elm
@@ -11,7 +11,7 @@ startVisitPage : Model -> Html Msg
 startVisitPage model =
     div [ class "w-60-ns center tc" ]
         [ div [ class "mt5 dib ba bw2 br-100 pa2 pt3 b-pink circle" ] [ img [ class "h5 ma3 mt4 rotate", src "./assets/small_logo.png" ] [] ]
-        , p [ class "brand f3 w60 mh1 tc pt4" ] [ text <| convertToMin model.timerLength ]
+        , p [ class "brand f3 w60 mh1 tc pt4 code " ] [ text <| convertToMin model.timerLength ]
         , section [ class "center ma0 mt2 pt2" ]
             [ a [ href "#stopVisitQuestionPage", class "link" ] [ button [ class "br1 h3 w7 ma3 brand bg-white center db f4 b-orange" ] [ text "Finish my visit" ] ]
             , section [ class "center ma0 mt2" ] [ button [ class "h3 w7 br1 ma3 brand-pink bg-white center db f4 br-pink ", onClick CancelVisit ] [ text "Cancel the visit" ] ]
--- src/elm/Routes/TextMessagePage.elm
+++ src/elm/Routes/TextMessagePage.elm
@@ -20,6 +20,6 @@ textMessagePage model =
     div [ class "w-60-ns center tc" ]
         [ div [ class "w-100 tl ma2 pa2" ] [ img [ class "h3", onClick GoBack, src "./assets/back_btn.svg" ] [] ]
         , img [ class "vh-15 pa3 br-100", src "./assets/write.svg" ] []
-        , div [ class "pa3" ] [ textarea [ class "vh-25 w-100 input-reset pa3 f4 br3 b-orange bg--black-40", onInput UpdateTextFeedback, value textValue, placeholder "Enter feedback here..." ] [] ]
+        , div [ class "pa3" ] [ textarea [ class "vh-25 w-100 input-reset pa3 f4 br3 b-orange listOfVisits", onInput UpdateTextFeedback, value textValue, placeholder "Enter feedback here..." ] [] ]
         , a [ href "#stopVisitPage" ] [ button [ class "dim b pointer w7 h8 br1 f3 ma2  bg--bp white" ] [ text "Submit" ] ]
         ]
